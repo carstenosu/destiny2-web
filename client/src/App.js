c99d6d7387f1daeefdc1515faf565e64d0cfc70d
foleyc <christopher.foley@vanderbilt.edu>
2019-10-20 17:02:13 +0900

    Initial setup of react client and express api

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/destiny")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.state.apiResponse}
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
