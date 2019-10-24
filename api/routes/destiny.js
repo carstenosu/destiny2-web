0189bd126581f8324fbfd06776f1b1989e94cf15
foleyc <christopher.foley@vanderbilt.edu>
2019-10-24 15:01:14 +0900

    Created destiny service

--- api/routes/destiny.js
+++ api/routes/destiny.js
@@ -2,23 +2,21 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios').default;
 const inspect = require('util').inspect;
-
-
-const instance = axios.create({
-   baseURL: 'https://www.bungie.net/Platform',
-   timeout: 1000,
-   headers: {'X-API-Key': process.env.DESTINY_API_KEY}
-});
+const destinyService = require('../services/destiny-service');
+const destinyClient = new destinyService.DestinyService();
 
 
 router.get('/', function(req, res, next) {
-   instance.get('/Destiny2/Manifest/')
+   //destinyClient.getManifest()
+   destinyClient.getProfile()
    .then((data)=>{
+
       console.log(data);
-      //res.send(data);
+
       res.json(inspect(data));
    })
    .catch((error) => {
+      console.log(error);
       res.send('There was an error')
    })
    .finally(() => {
---
+++ api/services/destiny-service.js
@@ -0,0 +1,27 @@
+const axios = require('axios').default;
+
+class DestinyService {
+
+   constructor( ) {
+      this.axiosClient = axios.create({
+         baseURL: 'https://www.bungie.net/Platform',
+         timeout: 1000,
+         headers: {'X-API-Key': process.env.DESTINY_API_KEY}
+      })
+   }
+
+   getManifest() {
+      return this.axiosClient.get('/Destiny2/Manifest/');
+   }
+
+   searchDestinyPlayer() {
+      return this.axiosClient.get('/Destiny2/SearchDestinyPlayer/1/carsten/');
+   }
+
+   getProfile( destinyMembershipId, membershipType) {
+      return this.axiosClient.get('/Destiny2/1/Profile/4611686018429730113/')
+   }
+
+}
+
+exports.DestinyService = DestinyService;
